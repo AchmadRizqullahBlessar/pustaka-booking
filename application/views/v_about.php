425bec7c92e1d5ee84d4e43b26ba24eab7a4ad0c
AchmadRizqullahBlessar <essar2002@gmail.com>
2022-10-01 21:07:19 +0700

    Menambahkan template sederhana dari pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Rizqullah</li>
+        <li>Nama Belakang : Blessar</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Planet ke 3 di tata surya</li>
+    </ul>
+    
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>e-sport</li>
+        <li>Angkat Galon</li>
+    </ul>
+</section>
