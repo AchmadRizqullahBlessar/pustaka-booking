28b1799129442ac1356a2d3290b5f69a9b09954a
AchmadRizqullahBlessar <essar2002@gmail.com>
2022-10-03 15:22:51 +0700

    mengembalikan file view

--- application/views/view-latihan1.php
+++ application/views/view-latihan1.php
@@ -1,6 +1,6 @@
 <html>
     <body>
-        Halo Kawan.. Yuk kita Maen<br>
+        Halo Kawan.. Yuk kita belajar Akutansi<br>
         Nilai 1 = <?= $nilai1; ?>
         Nilai 2 = <?= $nilai2; ?>
         ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
