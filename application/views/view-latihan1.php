325f2341277b53f8285f92946f279c4677093916
AchmadRizqullahBlessar <essar2002@gmail.com>
2022-10-03 15:17:16 +0700

    Mengubah belajar menjadi maen

--- application/views/view-latihan1.php
+++ application/views/view-latihan1.php
@@ -1,6 +1,6 @@
 <html>
     <body>
-        Halo Kawan.. Yuk kita belajar Akutansi<br>
+        Halo Kawan.. Yuk kita Maen<br>
         Nilai 1 = <?= $nilai1; ?>
         Nilai 2 = <?= $nilai2; ?>
         ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
